31003831b8caa70ba219d78a3082afd8a1b80a0f
Majaz <majazulhaque123@gmail.com>
2023-02-15 21:39:30 +0530

    add javascript function to run the timer and add responsiveness to the design

--- js/index.js
+++ js/index.js
@@ -1,36 +1,60 @@
-var startBtn = document.getElementById('start_btn');
-var stopBtn = document.getElementById('stop_btn');
-var resetBtn = document.getElementById('reset_btn');
-var timer = document.getElementById('text_timer');
-var minute = document.getElementById('minute');
-var second = document.getElementById('second');
-
-// Global Variable
-var min = 00;
-var sec = 00;
-var time = 1;
-// Function to start the timer.
-function startTimer(){
-    min++;
-    sec++;
-    minute.innerText = min;
-    second.innerText = sec;
-}
-
-// Function to stop the timer.
-function stopTimer(){
-    timer.innerText = "30";
-
-}
-
-// Function to reset the timer.
-function resetTimer(){
-    min = 00;
-    sec = 00;
-    minute.innerText = min;
-    second.innerText = sec;
-}
-
-// startBtn.addEventListener('click', startTimer());
-// stopBtn.addEventListener('click', stopTimer());
-// resetBtn.addEventListener('click', resetTimer());
+window.onload = function () {
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var millisecond = document.getElementById("milliseconds")
+    var appendSeconds = document.getElementById("seconds")
+    var startBtn = document.getElementById('start_btn');
+    var stopBtn = document.getElementById('stop_btn');
+    var resetBtn = document.getElementById('reset_btn');
+    var timer;
+  
+    startBtn.onclick = function() {
+      
+      clearInterval(timer);
+       timer = setInterval(startTimer, 10);
+    }
+    
+      stopBtn.onclick = function() {
+         clearInterval(timer);
+    }
+    
+  
+    resetBtn.onclick = function() {
+       clearInterval(timer);
+      tens = "00";
+        seconds = "00";
+      millisecond.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens <= 9){
+        millisecond.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        millisecond.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        millisecond.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    
+    }
+    
+  
+  }
